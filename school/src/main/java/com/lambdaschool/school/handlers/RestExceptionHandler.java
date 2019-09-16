302d46fff67c91563a800267285d7cb8f121c407
ren.meta@gmail.com <ren.meta@gmail.com>
2019-09-16 18:21:12 -0400

    non-handled endpoint is accessed error handled

--- school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
+++ school/src/main/java/com/lambdaschool/school/handlers/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -42,5 +43,16 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         errorDetail.setDetail(ex.getMessage());
         errorDetail.setDeveloperMessage(request.getDescription(true));
         return new ResponseEntity<>(errorDetail, null, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(ex.getMethod());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("Rest Handler Not Found (check for valid URI)");
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
     }
 }
